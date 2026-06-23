2ddc6fb1e31bda0684359e81fd8388730e4fad6c
jaiverma0923 <jaiverma2004@gmail.com>
2026-06-24 00:54:21 +0530

    testing

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,8 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request: NextRequest) {
     const token = await getToken({ req: request, secret: process.env.AUTH_SECRET });
     const url = request.nextUrl;
-    console.log("MIDDLEWARE HIT");
+    console.log("TOKEN:", token);
+    console.log("PATH:", request.nextUrl.pathname);
     if (
         token &&
         (
